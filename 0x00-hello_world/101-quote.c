e0edddf3ae5e800ce8653bc26b58689fc27ddd60
Ramatu Abubakar <rsmercy9@mail.com>
2023-08-10 15:57:23 -0700

    101-quote.c:7: warning: No description found for return value of main

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - A program that prints a line using printf function
  * Return 1 (Success)
  */
-int main(void)
+void main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
-} 
+}
