2ff9172c305b5f3abc6222838ac99d01e974d9c5
Ramatu Abubakar <rsmercy9@mail.com>
2023-08-10 16:15:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - A program that prints a line using printf function
  * Return 1 (Success)
  */
-int main(void)
+int void main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
