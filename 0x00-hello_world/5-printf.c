40ef1eec233d7bbe0192db6bed04d97966333776
Ramatu Abubakar <rsmercy9@mail.com>
2023-08-10 08:08:50 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using printf function
+ * Return: 0 (successful)
+ */
+int main(void)
+{       printf("with proper grammar, but the outcome is a piece of art,\n");
+Return: (0);
+}
