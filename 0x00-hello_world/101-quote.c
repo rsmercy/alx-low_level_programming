83a6fbd07c97c8066a0924b04d3e07884787ea30
Ramatu Abubakar <rsmercy9@mail.com>
2023-08-10 17:12:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A program that prints a line using printf function
- * Return 1 (Success)
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
 return (1);
 }
