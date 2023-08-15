21bb8a0197d68951bb5d9f2eb98feca80b6cdc1b
Ramatu Abubakar <rsmercy9@mail.com>
2023-08-15 11:02:59 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
-#include "../main.h"
+#include "main.h"
 
 /**
- * main - check the code for School students.
+ * main - Prints "Holberton" followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	print_alphabet();
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
 	return (0);
 }
