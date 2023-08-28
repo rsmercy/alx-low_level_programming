06e8b8ef5e91b5a4419048cafa835813b43e1919
Ramatu Abubakar <rsmercy9@mail.com>
2023-08-28 08:36:00 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,7 +7,8 @@
  *
  * Return: changed array with new value for n bytes
  */
-char *_memset(char *s, char b, unsigned int n)
+
+void *_memset(void *s, int c, size_t n)
 {
 	unsigned int index;
 	unsigned char *memory = s, value = c;
