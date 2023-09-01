0c47fdf5fd5ab6449d8f88afb5218e0a768e1d22
Ramatu Abubakar <rsmercy9@mail.com>
2023-09-01 15:47:07 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  *@argc: arguement count
+  *@argv: arguement array or vector
+  *
+  *Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", (argc - 1));
+	return (0);
+}
