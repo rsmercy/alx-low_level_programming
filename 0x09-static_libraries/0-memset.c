20cf9bc789710f6afaf6e48ea401e29332567627
Ramatu Abubakar <rsmercy9@mail.com>
2023-09-01 12:14:52 -0700

    update

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - Program fills a block of memory with a specific value
+ * @n: number of bytes to be changed
+ *
+ * Return: change array with new value for n bytes
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+
+int d = 0;
+
+for (; n > 0; a++)
+{
+	s[d] = b;
+	n--;
+}
+
+return (s);
+
+}
---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - function concatenate two strings
+ * using at most n bytes
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int a;
+int b;
+a = 0;
+while (dest[a] != '\0')
+{
+a++;
+}
+b = 0;
+while (b < n && src[b] != '\0')
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+dest[a] = '\0';
+return (dest);
+}
