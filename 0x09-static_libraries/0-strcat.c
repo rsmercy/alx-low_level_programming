49eca2e5b1cacc35ee4fde56b5f7367f93e1a46a
Ramatu Abubakar <rsmercy9@mail.com>
2023-09-01 14:30:47 -0700

    update

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,30 +1,30 @@
 #include "main.h"
 
 /**
- *  _strcat - function that concatenates two strings
- *  @dest: entered value
- *  @src: entered value
- *
- *  Return: void
- */
+  * _strcat - a function that concertinates two strings
+  *@dest: destination
+  *@src: source
+  *
+  *Return: final concertinated sting
+  */
 
 char *_strcat(char *dest, char *src)
+{
+	int count1 = 0;
+	int count2 = 0;
 
-{
-int a;
-int b;
-a = 0;
-while (dest[a] != '\0')
-{
-a++;
-}
-b = 0;
-while (src[b] != '\0')
-{
-dest[a] = src[b];
-a++;
-b++;
-}
-dest[a] = '\0';
-return (dest);
+	/* finding the length of string dest */
+	while (dest[count1] != '\0')
+	{
+		count1++;
+	}
+	while (src[count2] != '\0')
+	{
+		dest[count1] = src[count2];
+		/* count1 will start at the last value of string dest */
+		count1++;
+		count2++;
+	}
+	dest[count1] = '\0';
+	return (dest);
 }
