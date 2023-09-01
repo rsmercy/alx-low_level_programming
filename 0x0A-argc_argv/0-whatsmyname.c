340ecd3666549059bb0140e511ae0f7a74dc13d4
Ramatu Abubakar <rsmercy9@mail.com>
2023-09-01 15:45:00 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  *@argc: arguement count
+  *@argv: arguement array or vector
+  *
+  *Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
