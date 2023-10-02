d6663df4b46a7ec29f4c7ccb15e3984880228c58
Ramatu Abubakar <rsmercy9@gmail.com>
2023-10-02 07:03:39 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,8 +4,7 @@
 /**
  * read_textfile- Read text file print to STDOUT.
  * @filename: text file being read
- * Ramatu Abubakar
- *
+ * @letters: number of letters to be read
  * Return: w- actual number of bytes read and printed
  *        0 when function fails or filename is NULL.
  */
@@ -27,3 +26,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(fd);
 	return (w);
 }
+
